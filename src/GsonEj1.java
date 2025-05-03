83cf05ba025a2c61a5a5466f309049c872d3d020
Malena <Malena.Rodriguez@edu.uah.es>
2025-05-03 20:32:37 +0200

    Version final

--- src/GsonEj1.java
+++ src/GsonEj1.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 public class GsonEj1 {
     public static void main(String[] args){
         Gson gson=new Gson();
-
+        //Hoja1
         //crear objetos
         Circle circle=new Circle();
         Employee empleado=new Employee(1,"Malena","Rodriguez",1000);
@@ -21,8 +21,18 @@ public class GsonEj1 {
         GsonUtilEjemplo.cargarObjetoDesdeArchivo(rutaArchivo1,Circle.class);
         GsonUtilEjemplo.cargarObjetoDesdeArchivo(rutaArchivo2,Employee.class);
 
+        //Hoja2
+        Persona persona=new Persona("Lidia",18);
+        Estudiante estudiante=new Estudiante("Carmen",55);
 
+        String rutaArchivo3="personas.json";
+        String rutaArchivo4="estudiantes.json";
 
+        GsonUtilEjemplo.guardarObjetoEnArchivo(rutaArchivo3, persona);
+        GsonUtilEjemplo.guardarObjetoEnArchivo(rutaArchivo4, empleado);
+
+        GsonUtilEjemplo.cargarObjetoDesdeArchivo(rutaArchivo3,Persona.class);
+        GsonUtilEjemplo.cargarObjetoDesdeArchivo(rutaArchivo4,Employee.class);
 
     }
 }
